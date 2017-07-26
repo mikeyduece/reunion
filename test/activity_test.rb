875774db1d8389f55e921bab88564ff425dc980b
Mike Heft <mikeheft@gmail.com>
2017-07-26 09:04:55 -0600

    I made a thing with a name

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,17 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  attr_reader :activity
+
+  def setup
+    @activity = Activity.new("Brunch")
+  end
+
+  def test_its_a_thing
+    assert_instance_of Activity, activity
+  end
+  
+  def test_it_knows_its_activity_name
+    assert_equal "Brunch", activity.name
+  end
 end
