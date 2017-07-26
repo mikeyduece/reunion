1f84aab7a48403d15ea22ac29de2de50c0fd910f
Mike Heft <mikeheft@gmail.com>
2017-07-26 09:27:33 -0600

    work on reunion

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -1,2 +1,8 @@
 class Reunion
+  attr_reader :city
+
+  def initialize(city=nil)
+    @city = city
+
+  end
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -4,4 +4,9 @@ require 'minitest/autorun'
 require './lib/reunion'
 
 class ReunionTest < Minitest::Test
+
+  def test_its_a_thing
+    reunion = Reunion.new("Denver")
+    assert_instance_of Reunion, reunion
+  end
 end
